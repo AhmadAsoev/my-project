6dfdc5a7281fb39e7a50c308669e3ab8179fd8da
AhmadAsoev <asoev.ahmad@mail.ru>
2024-12-10 02:10:53 +0500

    Add loading and Error from  Api using Axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
-import { Product } from "./components/product"
-import { products } from "./data/products"
+import { useEffect, useState } from 'react';
+import { Product } from './components/product';
+import axios, { AxiosError } from 'axios';
+import { IProduct } from './models';
 
 function App() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  async function fetchProducts() {
+    try {
+      setError('');
+      setLoading(true);
+      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5');
+      setProducts(response.data);
+      setLoading(false);
+    } catch (e: unknown) {
+      const error = e as AxiosError;
+      setLoading(false);
+      setError(error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <div className="container mx-auto max-w-2xl pt-5">
-        <Product product={products[0]} />
-        <Product product={products[1]} />
+        {loading && <p className="text-center text-green-600">Loading...</p>}
+        {error && <p className="text-center text-red-600">{error}</p>}
+        {products.map((product) => (
+          <Product product={product} key={product.id} />
+        ))}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
