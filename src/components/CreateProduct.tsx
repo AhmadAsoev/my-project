11c77bc590ed2125e7bbe5a63d2c8a94b8a394b5
AhmadAsoev <asoev.ahmad@mail.ru>
2024-12-11 20:47:43 +0500

    Add onChange &&input

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,10 +1,32 @@
 import { useState } from 'react';
+import { IProduct } from '../models';
+import axios from 'axios';
+
+const productData: IProduct = {
+  title: '',
+  price: 13.5,
+  description: 'lorem ipsum set',
+  image: 'https://i.pravatar.cc',
+  category: 'electronic',
+  rating: {
+    rate: 42,
+    count: 10,
+  },
+};
 
 export function CreateProduct() {
   const [value, setValue] = useState('');
 
-  const submitHandler = (event: React.FormEvent) => {
-    event.preventDefault();
+  const submitHandler = async (event: React.FormEvent) => {
+      event.preventDefault();
+      productData.title = value
+      
+     const response = await axios.post<IProduct>('https://fakestoreapi.com/products', productData)
+  };
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+    console.log(event.target.value);
   };
 
   return (
@@ -12,8 +34,9 @@ export function CreateProduct() {
       <input
         type="text"
         className="border py-2 px-4 mb-2 w-full outline-0"
-              placeholder="Ented product title..."
-              value={value}
+        placeholder="Ented product title..."
+        value={value}
+        onChange={changeHandler}
       />
       <button type="submit" className="py-2 px-4 border bg-yellow-400 hover:text-white">
         Create
